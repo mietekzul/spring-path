469beb632e53c35601340fd2f9f4db407ff82140
Daniel Ostapiuk <daniel.ostapiuk@commerzsystems.com>
2017-10-04 13:22:29 +0200

    Added example of spring boot tests.

--- spring-boot/spring-boot-configuration/src/main/java/pl/raziel/springbootconfiguraiton/SpringBootConfiguraitonApplication.java
+++ spring-boot/spring-boot-configuration/src/main/java/pl/raziel/springbootconfiguraiton/SpringBootConfiguraitonApplication.java
@@ -1,19 +1,73 @@
 package pl.raziel.springbootconfiguraiton;
 
-import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 
-import java.io.PrintStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
 
 @SpringBootApplication
 public class SpringBootConfiguraitonApplication {
 
-    public static void main(String[] args) {
+    Logger log = LoggerFactory.getLogger(SpringBootConfiguraitonApplication.class);
 
-        SpringApplication app = new SpringApplication(SpringBootConfiguraitonApplication.class);
-        app.setBanner((environment, sourceClass, out) -> out.print("\n\n\tThis is my own banner!\n\n".toUpperCase()));
-        app.run(args);
+    public static void main(String[] args) {
+//		SpringApplication.run(SpringBootConfiguraitonApplication.class, args);
+        new SpringApplicationBuilder(SpringBootConfiguraitonApplication.class)/*.listeners(e -> log.info("### > " + e.getClass().getCanonicalName()))*/.run(args);
+    }
+
+
+    @Value("${server.ip}")
+    String serverIp;
+
+    @Autowired
+    MyAppProperties props;
+
+    @Bean
+    CommandLineRunner values() {
+        return args -> {
+            log.info(" > The Server IP is: " + serverIp);
+            log.info(" > App Name: " + props.getName());
+            log.info(" > App Info: " + props.getDescription());
+        };
+    }
+
+    @Component
+    @ConfigurationProperties(prefix = "myapp")
+    public static class MyAppProperties {
+        private String name;
+        private String description;
+        private String serverIp;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getServerIp() {
+            return serverIp;
+        }
+
+        public void setServerIp(String serverIp) {
+            this.serverIp = serverIp;
+        }
     }
 }
---
+++ spring-boot/spring-boot-web-test/src/main/java/pl/raziel/springbootwebtest/SpringBootWebTestApplication.java
@@ -0,0 +1,12 @@
+package pl.raziel.springbootwebtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebTestApplication.class, args);
+	}
+}
