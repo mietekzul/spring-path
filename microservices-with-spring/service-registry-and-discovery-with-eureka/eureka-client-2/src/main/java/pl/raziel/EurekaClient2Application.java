2830669b81c7db4f086f06cfab84817756ac41fe
Daniel Ostapiuk <daniel.ostapiuk@commerzsystems.com>
2017-10-12 10:51:54 +0200

    Registering Eureka clients. Service discovery with rest template.

--- microservices-with-spring/service-registry-and-discovery-with-eureka/eureka-client-2/src/main/java/pl/raziel/EurekaClient2Application.java
+++ microservices-with-spring/service-registry-and-discovery-with-eureka/eureka-client-2/src/main/java/pl/raziel/EurekaClient2Application.java
@@ -1,28 +1,25 @@
 package pl.raziel;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class EurekaClient2Application {
 
-    @Qualifier("eurekaClient")
     @Autowired
     private EurekaClient client;
 
     @RequestMapping("/serviceinfo")
     public String serviceInfo() {
-        InstanceInfo instance = client.getNextServerFromEureka("myClient", false);
+        InstanceInfo instance = client.getNextServerFromEureka("myOtherClient", false);
         return instance.getHomePageUrl();
     }
 
